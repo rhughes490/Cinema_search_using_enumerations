6aa70543ccc756cfee7560d4e447bb8663ab8856
Ronan Hughes <rhughes490@gmail.com>
2020-06-17 14:27:54 +0100

    Six passing tests

--- models/cinema.js
+++ models/cinema.js
@@ -19,6 +19,12 @@ const Cinema = function (films) {
     })
   }
 
+  this.filterByYear = function (year) {
+    return films.filter(film => {
+      return film.year == year
+    })
+  }
+
 }
 
 module.exports = Cinema;
--- specs/cinema_spec.js
+++ specs/cinema_spec.js
@@ -44,8 +44,16 @@ describe('Cinema', function () {
     assert.deepStrictEqual(actual, [moonlight, trainspotting])
   });
 
-  xit('should be able to check whether there are some films from a particular year');
-  xit('should be able to check whether there are no films from a particular year');
+  it('should be able to check whether there are some films from a particular year', function () {
+    const actual = cinema.filterByYear(2016);
+    assert.deepStrictEqual(actual, [moonlight]);
+  });
+
+  it('should be able to check whether there are no films from a particular year', function () {
+    const actual = cinema.filterByYear(2019);
+    assert.deepStrictEqual(actual, []);
+  });
+
   xit('should be able to check whether all films are over a particular length');
   xit('should be able to calculate total running time of all films');
 
